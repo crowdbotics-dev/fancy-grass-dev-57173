776e42c7bf251fa3ac0813448dda5992f2501870
Crowdbotics <engineering@crowdbotics.com>
2023-01-16 07:48:23 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/fancy_grass_dev_57173/MainActivity.java
@@ -0,0 +1,15 @@
+package com.fancy_grass_dev_57173;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "fancy_grass_dev_57173";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "fancy-grass-dev-57173",
+  url: "https://fancy-grass-dev-57173.botics.co",
+  api: "https://fancy-grass-dev-57173.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
